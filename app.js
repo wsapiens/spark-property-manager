ab81bc5f7826eefbe9f3c13dd6a08c188b6db319
Steve <wsapiens@hotmail.com>
2018-04-14 02:27:53 -0500

    first view by ejs

--- app.js
+++ app.js
@@ -12,13 +12,17 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  express.static(path.join(__dirname, 'node_modules')),
+  express.static(path.join(__dirname, 'public'))
+);
+
 
 app.use('/', indexRouter);
 app.use('/expenses', expensesRouter);
