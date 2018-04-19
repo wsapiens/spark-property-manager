d0773a7b47e7017281597a6df834e6223c88953c
Steve <wsapiens@hotmail.com>
2018-04-18 19:59:46 -0500

    upload folder

--- routes/file.js
+++ routes/file.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var multer = require('multer');
-var upload = multer({
-  dest: __dirname + '../public/receipts/',
-});
+var upload = multer({ dest: __dirname + './public/uploads/' });
 
-router.post('/receipt', upload.single('photo'), function(req, res, next) {
+router.post('/receipt', upload.single('receipt'), function(req, res, next) {
+  console.log('>>>>>>> ' + req.file);
+  console.log('>>>>>>> ' + req.files);
+
   if (!req.files)
     return res.status(400).send('No files were uploaded.');
 
-  let pic = req.files.photo;
+  let pic = req.files.reciept;
 
   // Use the mv() method to place the file somewhere on your server
   // sampleFile.mv('/somewhere/on/your/server/filename.jpg', function(err) {
