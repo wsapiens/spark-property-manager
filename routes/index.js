97026c562dc9f4afd471df2844719452010f50da
Steve <wsapiens@hotmail.com>
2018-05-01 00:53:45 -0500

    subscribe functionality

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const db = require('../db');
 const log = require('../log');
+const email = require('../email');
+const config = require('../config');
+const cryptoRandomString = require('crypto-random-string')
 var express = require('express');
 var router = express.Router();
 
@@ -98,6 +101,72 @@ router.get('/logout', function(req, res, next) {
       }
     });
   }
+});
+
+router.get('/subscribe', function(req, res, next) {
+  res.render('subscribe', { message: '', error_message: ''});
+});
+
+router.post('/subscribe', function(req, res, next) {
+  if(!req.body.email) {
+    return res.render('subscribe', { message: '', error_message: 'email address is missing!'});
+  }
+
+  db.query('SELECT * FROM login_user WHERE email=$1', [req.body.email])
+    .then(rs => {
+      if(rs.rows.length >0) {
+        return res.render('subscribe', { message: '', error_message: 'The email is already registered!'});
+      }
+    }).catch(e => { // if error for checking email
+      console.error(e.stack);
+      log.error(e.stack);
+      res.send(e.stack);
+    });
+
+  db.query('INSERT INTO company (name) VALUES ($1) RETURNING id', [req.body.email])
+    .then(rs => {
+      var company_id = rs.rows[0]['id'];
+      var random_password = cryptoRandomString(5);
+      var message	= {
+            text:	'Your Account has been created with your email: '
+                + req.body.email
+                + ' and temporary password: '
+                + random_password
+                + ', please change password after login ' + config.get('app.url'),
+                from:	'SPARK PM <' + config.get('smtp.username') + '>',
+                to:		' <' + req.body.email +'>',
+                //cc:		"else <else@your-email.com>",
+                subject:	'SPARK Property Manager App Account Creation',
+                // attachment:
+                // [
+                //    {data:"<html>i <i>hope</i> this works!</html>", alternative:true},
+                //    {path:"path/to/file.zip", type:"application/zip", name:"renamed.zip"}
+                // ]
+          };
+        if(!company_id || company_id < 1) {
+          return res.render('subscribe', { message: '', error_message: 'Account Creation Fail!'});
+        }
+
+        db.query("INSERT INTO login_user (email, password, is_manager, company_id) VALUES ($1, crypt($2, gen_salt('bf')), $3, $4)",
+                  [ req.body.email,
+                    random_password,
+                    true,
+                    company_id ])
+          .then(rs => {
+            email.send(message, function(err, message) { log.info(err || message); });
+          }).catch(e => { // if error for creating user
+            console.error(e.stack);
+            log.error(e.stack);
+            res.send(e.stack);
+          });
+
+    }).catch(e => { // if error for creating company
+      console.error(e.stack);
+      log.error(e.stack);
+      res.send(e.stack);
+    });
+
+    return res.render('subscribe', { message: 'account has been created', error_message: ''});
 });
 
 module.exports = router;
