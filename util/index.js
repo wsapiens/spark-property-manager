94cb27c32bf03cbebf1fc82ab95f4665e761828f
Steve <wsapiens@hotmail.com>
2018-06-26 22:13:08 -0500

    better modularization

--- util/index.js
+++ util/index.js
@@ -1,28 +1,27 @@
-module.exports.getImportAmount =
-function getImportAmount(amount, filter) {
-  // return
-  if(filter.match(/return/i) && 0 < amount) {
+module.exports = {
+  getImportAmount: function getImportAmount(amount, filter) {
+    // return
+    if(filter.match(/return/i) && 0 < amount) {
+        return -1.0 * amount;
+    }
+
+    // sale
+    if( !filter.match(/return/i) && 0 > amount) {
       return -1.0 * amount;
-  }
+    }
+    return amount;
+  },
 
-  // sale
-  if( !filter.match(/return/i) && 0 > amount) {
-    return -1.0 * amount;
-  }
-  return amount;
-};
+  getImportDescription: function getImportDescription(description, filter) {
+    if(filter.match(/return/i)) {
+      return description + '<mark>[Return]</mark>';
+    }
+    return description;
+  },
 
-module.exports.getImportDescription =
-function getImportDescription(description, filter) {
-  if(filter.match(/return/i)) {
-    return description + '<mark>[Return]</mark>';
+  getRandomRGB: function getRandomRGB() {
+    return ["rgb(" + Math.floor(Math.random() * 255),
+              Math.floor(Math.random() * 255),
+              Math.floor(Math.random() * 255) + ")"].join(",");
   }
-  return description;
-};
-
-module.exports.getRandomRGB =
-function getRandomRGB() {
-  return ["rgb(" + Math.floor(Math.random() * 255),
-            Math.floor(Math.random() * 255),
-            Math.floor(Math.random() * 255) + ")"].join(",");
 };
