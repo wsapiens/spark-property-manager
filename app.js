9955348afc7518e9ae4e460d4fd73279a9c2c233
Steve <wsapiens@hotmail.com>
2022-01-12 13:33:16 -0800

    fix build warning for missing semicolon

--- app.js
+++ app.js
@@ -154,9 +154,9 @@ const limiter = rateLimit({
 	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-})
+});
 
 // Apply the rate limiting middleware to all requests
-app.use(limiter)
+app.use(limiter);
 
 module.exports = app;
