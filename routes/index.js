1438c59a13c28b0adf70554898a6860fa37ccec2
Steve <wsapiens@hotmail.com>
2022-07-27 21:24:31 -0700

    upgrade passport version to 0.6.0

--- routes/index.js
+++ routes/index.js
@@ -89,14 +89,17 @@ router.post('/password', function(req, res, next) {
 router.get('/logout', function(req, res, next) {
   if (req.session) {
     // delete session object
-    req.session.destroy(function(err) {
-      if(err) {
-        log.error(err);
+    req.logout(function(err) {
+      if (err) {
         return next(err);
-      } else {
-        req.logout();
-        return res.redirect('/');
       }
+      req.session.destroy(function(err) {
+        if(err) {
+          log.error(err);
+          return next(err);
+        }
+      });
+      return res.redirect('/');
     });
   }
 });
